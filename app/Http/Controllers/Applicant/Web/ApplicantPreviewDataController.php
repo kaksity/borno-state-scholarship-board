dcbaedbab437bd22b9f5f0f880f2a65e566d290d
Dauda Pona <daudapona2@gmail.com>
2023-08-22 08:35:42 +0100

    feat: added remita payment

--- app/Http/Controllers/Applicant/Web/ApplicantPreviewDataController.php
+++ app/Http/Controllers/Applicant/Web/ApplicantPreviewDataController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers\Applicant\Web;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Applicant\Web\BioData\UpdateApplicantBioDataRequest;
+use App\Services\Interfaces\ApplicantPaymentDataServiceInterface;
+use App\Services\Interfaces\RemitaServiceInterface;
 use App\Services\Interfaces\ApplicantBioDataServiceInterface;
 use App\Services\Interfaces\ApplicantSchoolDataServiceInterface;
 use App\Services\Interfaces\ApplicantServiceInterface;
 use App\Services\Interfaces\CountryServiceInterface;
 use App\Services\Interfaces\LgaServiceInterface;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -20,6 +23,8 @@ class ApplicantPreviewDataController extends Controller
         private ApplicantServiceInterface $applicantServiceInterface,
         private ApplicantBioDataServiceInterface $applicantBioDataServiceInterface,
         private ApplicantSchoolDataServiceInterface $applicantSchoolDataServiceInterface,
+        private ApplicantPaymentDataServiceInterface $applicantPaymentDataServiceInterface,
+        private RemitaServiceInterface $remitaServiceInterface,
     )
     {
     }
@@ -62,11 +67,48 @@ class ApplicantPreviewDataController extends Controller
     {
         
         $loggedInApplicant = auth('applicant')->user();
+
+        $applicantPayment = $this->applicantPaymentDataServiceInterface->getApplicantPaymentDataFiltered([
+            'applicant_id' => $loggedInApplicant->id,
+        ]);
         
-        $this->applicantServiceInterface->updateApplicantRecord([
-            'status' => 'Submitted'
-        ], $loggedInApplicant->id);
+        if (empty($applicantPayment)) {
+            return redirect()->route(
+                'applicant.applicant-payment-data.index'
+            )->with('status', 'You must complete the payment step before proceeding');
+        }
+
+        $applicantPaymentPaymentData = $applicantPayment[0] ?? null;
+
+        if (is_null($applicantPaymentPaymentData)) {
+            return redirect()->route(
+                'applicant.applicant-payment-data.index'
+            )->with('status', 'You must complete the payment step before proceeding');
+        }
+
+        if ($applicantPaymentPaymentData->status === 'paid') {
+            $this->applicantServiceInterface->updateApplicantRecord([
+                'status' => 'Submitted'
+            ], $loggedInApplicant->id);
+            
+            return back()->with('status', 'Application for Scholarship has been submitted successfully');
+        }
+
+        $response = $this->remitaServiceInterface->verifyPayment([
+            'rrr' => $applicantPaymentPaymentData->rrr
+        ]);
         
-        return back()->with('status', 'Application for Scholarship has been submitted successfully');
+        if ($response->status === '00') {
+            $this->applicantPaymentDataServiceInterface->updateApplicantPaymentDataRecord([
+                'completed_payment_at' => Carbon::now(),
+                'status' => 'paid'
+            ], $applicantPaymentPaymentData->id);
+
+            return back()->with('status', 'Application for Scholarship has been submitted successfully');
+        }
+
+        return redirect()->route(
+            'applicant.applicant-payment-data.index'
+        )->with('status', 'You must complete the payment step before proceeding');
     }
 }
